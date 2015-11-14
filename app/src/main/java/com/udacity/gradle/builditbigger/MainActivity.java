a925f2d09efe88e6c8c45d0f22fd06cfca7d913e
Kacyn Fujii <kacyn.fujii@gmail.com>
2015-11-13 21:47:20 -0800

    finished step 3

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,17 +2,25 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.example.kacyn.displayjokelib.DisplayJokeActivity;
+import com.example.kacyn.myapplication.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    String mJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +52,61 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view){
-        Intent jokeIntent = new Intent(this, DisplayJokeActivity.class);
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
-        //jokeIntent.putExtra(getString(R.string.joke_key), new JokeProvider().getJoke());
-        startActivity(jokeIntent);
+
+        new EndpointsAsyncTask(this).execute();
 
         //Toast.makeText(this, new JokeProvider().getJoke(), Toast.LENGTH_SHORT).show();
     }
+
+    class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+        private MyApi myApiService = null;
+        private Context mContext;
+
+        EndpointsAsyncTask(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            if(myApiService == null) {  // Only do this once
+//            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+//                    new AndroidJsonFactory(), null)
+//                    // options for running against local devappserver
+//                    // - 10.0.2.2 is localhost's IP address in Android emulator
+//                    // - turn off compression when running against local devappserver
+//                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+//                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+//                        @Override
+//                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+//                            abstractGoogleClientRequest.setDisableGZipContent(true);
+//                        }
+//                    });
+
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://builditbigger-1129.appspot.com/_ah/api/");
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            try {
+                return myApiService.getJoke().execute().getData();
+            }
+            catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mJoke = result;
+            Log.v("MainActivity", "mjoke in postexecute: " + mJoke);
+
+            Intent jokeIntent = new Intent(mContext, DisplayJokeActivity.class);
+            Log.v("MainActivity", "mjoke: " + result);
+            jokeIntent.putExtra(getString(R.string.joke_key), result);
+            startActivity(jokeIntent);
+        }
+    }
 }
+
--- backend/src/main/java/com/example/Kacyn/myapplication/backend/MyEndpoint.java
+++ backend/src/main/java/com/example/Kacyn/myapplication/backend/MyEndpoint.java
@@ -6,11 +6,10 @@
 
 package com.example.Kacyn.myapplication.backend;
 
+import com.example.JokeProvider;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
-import javax.inject.Named;
 
 /**
  * An endpoint class we are exposing
@@ -27,12 +26,13 @@ import javax.inject.Named;
 public class MyEndpoint {
 
     /**
-     * A simple endpoint method that takes a name and says Hi back
+     * A simple endpoint that retrieves joke from java library
      */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+//        response.setData("test");
+        response.setData("" + new JokeProvider().getJoke());
 
         return response;
     }
